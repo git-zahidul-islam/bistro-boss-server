10ad36a8780297d9c91ebba51481aa422821847a
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-05-21 05:59:20 +0600

    review menu cart added

--- index.js
+++ index.js
@@ -8,6 +8,55 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
+// mongodb data 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://bossUser:NMFCCAKtFF8LsSGY@cluster0.oy4gwmh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        const menuCollection = client.db('bistroDB').collection('menu')
+        const reviewCollection = client.db('bistroDB').collection('reviews')
+        const cartCollection = client.db('bistroDB').collection('cart')
+
+        app.get('/menu',async(req,res)=>{
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/reviews',async(req,res)=>{
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+        // post api method
+        app.post('/carts',async(req,res)=>{
+            const carts = req.body;
+            const result = await cartCollection.insertOne(carts)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 // server
 app.get('/',(req,res) =>{
     res.send('the bistro data server is running........')
