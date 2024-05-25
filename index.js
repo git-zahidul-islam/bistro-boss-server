1efef6b4d22cfb00308f4305692c0e1c03c35117
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-05-25 20:50:15 +0600

    update item api make done

--- index.js
+++ index.js
@@ -42,37 +42,47 @@ async function run() {
         const verifyToken = (req, res, next) => {
             console.log("inserted token", req.headers.authorization);
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'forbidden access' })
+                return res.status(401).send({ message: 'Unauthorized access' })
             }
             const token = req.headers.authorization.split(' ')[1]
             jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
                 if (err) {
-                    return res.status(401).send({ message: 'Forbidden access' })
+                    return res.status(401).send({ message: 'Unauthorized access' })
                 }
                 req.decoded = decoded;
                 next()
             })
         }
+        // admin verify
+        const adminVerify = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+            next()
+        }
 
         // user all api is here
-        app.get('/users', verifyToken, async (req, res) => {
+        app.get('/users', verifyToken, adminVerify, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
 
         app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
-            if (email !== req.decoded.email){
-                return res.status(403).send({message: "unauthorized"})
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "forbidden access" })
             }
-
-            const query = {email: email}
+            const query = { email: email }
             const user = await userCollection.findOne(query)
             let admin = false
-            if(user){
+            if (user) {
                 admin = user.role === 'admin'
             }
-            res.send({admin})
+            res.send({ admin })
         })
 
         app.post('/users', async (req, res) => {
@@ -87,7 +97,7 @@ async function run() {
             res.send(result)
         })
 
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', verifyToken, adminVerify, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -99,7 +109,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id', verifyToken, adminVerify, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query)
@@ -111,6 +121,40 @@ async function run() {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
+
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.patch('/menu/:id',async(req,res)=>{
+            const item = req.body;
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                    ...item
+                }
+            }
+            const result = await menuCollection.updateOne(filter,updateDoc)
+            res.send(result)
+        })
+
+        app.post('/menu', verifyToken, adminVerify, async (req, res) => {
+            const menu = req.body;
+            const result = await menuCollection.insertOne(menu)
+            res.send(result)
+        })
+
+        app.delete('/menu/:id', verifyToken, adminVerify, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
+
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
